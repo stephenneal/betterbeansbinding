67aaeb900cb0ca9d729814fbc8e9ab89807613ec
fabriziogiudici <none@none>
2009-04-25 17:32:40 +0200

    [svn r24] Fixed copyright headers for .java files.
    --HG--
    branch : src

--- core/src/main/java/org/jdesktop/el/ListELResolver.java
+++ core/src/main/java/org/jdesktop/el/ListELResolver.java
@@ -1,8 +1,32 @@
-/*
- * Copyright (C) 2007 Sun Microsystems, Inc. All rights reserved. Use is
- * subject to license terms.
- */
-
+/***********************************************************************************************************************
+ * 
+ * BetterBeansBinding - keeping JavaBeans in sync
+ * ==============================================
+ * 
+ * Copyright (C) 2009 by Tidalwave s.a.s. (http://www.tidalwave.it)
+ * http://betterbeansbinding.kenai.com
+ * 
+ * This is derived work from BeansBinding: http://beansbinding.dev.java.net
+ * BeansBinding is copyrighted (C) by Sun Microsystems, Inc.
+ * 
+ ***********************************************************************************************************************
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General 
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) 
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to 
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ ***********************************************************************************************************************
+ * 
+ * $Id: $
+ * 
+ **********************************************************************************************************************/
 package org.jdesktop.el;
 
 import java.util.List;
