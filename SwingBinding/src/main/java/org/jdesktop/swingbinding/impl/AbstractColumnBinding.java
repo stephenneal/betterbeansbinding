bee6f31c82fab0f9f33564a732f110b525dd38a1
fabriziogiudici <none@none>
2009-06-12 16:40:51 +0200

    [svn r66] Formatted, fixed imports.
    --HG--
    branch : src

--- SwingBinding/src/main/java/org/jdesktop/swingbinding/impl/AbstractColumnBinding.java
+++ SwingBinding/src/main/java/org/jdesktop/swingbinding/impl/AbstractColumnBinding.java
@@ -24,22 +24,23 @@
  *
  ***********************************************************************************************************************
  *
- * $Id: AbstractColumnBinding.java 60 2009-04-26 20:47:20Z fabriziogiudici $
+ * $Id: AbstractColumnBinding.java 66 2009-06-12 14:40:51Z fabriziogiudici $
  *
  **********************************************************************************************************************/
 package org.jdesktop.swingbinding.impl;
 
-import org.jdesktop.beansbinding.*;
-
+import org.jdesktop.beansbinding.Binding;
+import org.jdesktop.beansbinding.Property;
 
 /**
  * @author Shannon Hickey
  */
 public abstract class AbstractColumnBinding extends Binding {
+
     private int column;
 
     public AbstractColumnBinding(int column, Property columnSource,
-        Property columnTarget, String name) {
+            Property columnTarget, String name) {
         super(null, columnSource, null, columnTarget, name);
         this.column = column;
         setManaged(true);
